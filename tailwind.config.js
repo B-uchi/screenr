aaceed6dfde55eb262bea1089636696929167afe
Umeano Ibuchukwu <ibumeano@gmail.com>
2024-06-08 23:25:24 +0100

    Starter

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/components/**/*.jsx","./src/pages/**/*.jsx", "./src/*.{html,jsx}", "./index.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
