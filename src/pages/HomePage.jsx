9b774c9d59e53fbe5142e5e0119d74b48a043523
Umeano Ibuchukwu <ibumeano@gmail.com>
2024-06-09 14:10:08 +0100

    added file upload

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,11 +3,34 @@ import { IoCloudUploadOutline } from "react-icons/io5";
 import { connect } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { Toaster, toast } from "sonner";
+import { IoMdClose } from "react-icons/io";
 
 const Homepage = ({ currentUser }) => {
   const [jobDescription, setJobDescription] = useState("");
   const [uploadModal, setUploadModal] = useState(false);
+  const [selectedFiles, setSelectedFiles] = useState([]);
+  const [changeButtonText, setChangeButtonText] = useState(false);
   const navigate = useNavigate();
+
+  const handleFileChange = (event) => {
+    const newFiles = event.target.files;
+
+    if (selectedFiles.length + newFiles.length > 10) {
+      alert("You can only upload a maximum of 10 files.");
+      return;
+    }
+
+    const validFiles = Array.from(newFiles).filter(
+      (file) => file.type === "application/pdf"
+    );
+
+    setSelectedFiles([...selectedFiles, ...validFiles]);
+  };
+
+  const handleUpload = () => {
+    setChangeButtonText(true);
+    console.log("Uploading files:", selectedFiles);
+  };
 
   const analyzeCV = () => {
     if (currentUser) {
@@ -49,9 +72,17 @@ const Homepage = ({ currentUser }) => {
             <div className="mt-3 w-full flex flex-col items-center gap-3.5">
               <button
                 onClick={() => setUploadModal(true)}
-                className="transition-all flex items-center gap-2 justify-center hover:scale-105 rounded-lg bg-[#313131] text-white p-3 w-[300px]"
+                className="transition-all hover:scale-105 rounded-lg bg-[#313131] text-white p-3 w-[300px]"
               >
-                <IoCloudUploadOutline size={25} /> Upload Some CV
+                {changeButtonText ? (
+                  <div className="flex items-center gap-2 justify-center">
+                    <IoCloudUploadOutline size={25} /> {selectedFiles.length} CV uploaded
+                  </div>
+                ) : (
+                  <div className="flex items-center gap-2 justify-center">
+                    <IoCloudUploadOutline size={25} /> Upload Some CV
+                  </div>
+                )}
               </button>
               <button
                 onClick={() => analyzeCV()}
@@ -65,15 +96,31 @@ const Homepage = ({ currentUser }) => {
       </div>
       {uploadModal && (
         <div className="absolute flex flex-col justify-center items-center bg-slate-600 bg-opacity-70 w-full h-[92vh]">
-          <div className="text-center bg-white p-5 rounded-md">
-            {" "}
+          <div className="text-center relative bg-white p-5 rounded-md">
+            <button
+              onClick={() => {
+                setUploadModal(false);
+              }}
+              className="absolute right-3 text-red-600"
+            >
+              <IoMdClose />
+            </button>{" "}
             <input
               type="file"
               accept=".pdf"
               multiple
-              // onChange={handleFileChange}
+              onChange={handleFileChange}
             />
             <p>(Max: 10)</p>
+            <button
+              onClick={() => {
+                handleUpload();
+                setUploadModal(false);
+              }}
+              className="mt-2 bg-[#333333] text-white p-2 rounded-md"
+            >
+              Continue
+            </button>
           </div>
         </div>
       )}
